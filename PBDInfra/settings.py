91655052e2839d81ee07b6af3382105f60ebf7e4
VIPINRAJK26 <vipinrajk026@gmail.com>
2026-05-27 10:26:01 +0530

    added gitignore

--- PBDInfra/settings.py
+++ PBDInfra/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v0kyis&=1!t5h5pxln1ridt56bm89bj_9k#)oqq9jbt++qnqn7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
